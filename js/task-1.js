0cefbf7b53dc8c78deb734b1fcd4427a4f200bb2
OLHA55 <bagira2310@gmail.com>
2024-03-08 23:53:48 +0200

    js07neew002

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,16 @@
-const item = document.querySelector('ul#categories');
-console.log(item);
+ const item = document.querySelectorAll(".item")
+
+ console.log(`Number of categories:${item.length} `);
+const ul = Array.from(document.querySelector('#categories').children);
+for (let item of ul) {
+    let category = item.firstElementChild.textContent;   
+    let quantityElem = item.lastElementChild.children.length;
+    console.log(`Category: ${category} 
+    Elements:  ${quantityElem}`);
+};  
+
+
+
+
+
 
